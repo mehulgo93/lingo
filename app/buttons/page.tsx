517b055b14fa1aacb62f3012f0525a13f3752bb6
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-03-18 14:42:25 -0400

    adding different button variants

---
+++ app/buttons/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+
+const ButtonsPage = () => {
+  return (
+    // these are some of the tailwind classes used for this
+    // added some new variants for buttons in classes
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="sidebar">Sidebar</Button>
+      <Button variant="sidebarOutline">Sidebar Outline</Button>
+    </div>
+  );
+};
+
+export default ButtonsPage;
