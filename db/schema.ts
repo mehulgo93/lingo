dd0062043cfb53bdc2258153cb1456003488b96d
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-03-21 21:26:53 -0400

    updating schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,16 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
     id: serial("id").primaryKey(),
     title: text("title").notNull(),
     imageSrc: text("image_src").notNull(),
+});
+
+export const userProgress = pgTable("user_progress", {
+    userId: text("user_id").primaryKey(),
+    userName: text("user_name").notNull(),
+    userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+    activeCourseId:  integer("active_course_id").references(() => courses.id, {onDelete: "cascade"}),
+    hearts: integer("hearts").notNull().default(5),
+    points: integer("points").notNull().default(0),
 });
