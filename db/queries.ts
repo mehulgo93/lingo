c99e2aa1d5f39ca89522c9f48710e50c0d4aaabd
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-03-23 22:47:15 -0400

    adding more schema for courses and also adding loading states

--- db/queries.ts
+++ db/queries.ts
@@ -1,9 +1,39 @@
 // this function will run to call a query to get Courses from the drizzle database
 import { cache } from "react";
 import db from "./drizzle";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { courses, userProgress } from "./schema";
+
+
+export const getUserProgress = cache(async () => {
+    const {userId} = await auth();
+
+    if (!userId) {
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true
+        },
+    });
+
+    return data;
+})
 
 export const getCourses = cache(async() => {
     const data = await db.query.courses.findMany();
+
+    return data;
+})
+
+export const getCoursesById = cache(async(courseId: number) => {
+    const data = await db.query.courses.findFirst({
+        where: eq(courses.id, courseId),
+        // TODO: populate units and lessons
+    });
 
     return data;
 })
