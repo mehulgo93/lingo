f95e22bc06dc8dc39f5c490f3a5a1c6b98958fbc
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-03-25 19:57:55 -0400

    seed script

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -11,7 +11,7 @@ const LearnPage = async () => {
   const userProgressData = getUserProgress();
 
   const [userProgress] = await Promise.all([userProgressData]);
-
+  // the reason to introduce the if statement is here because we don't need to use terinary operator later on in each comoponent
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
@@ -19,14 +19,15 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          //TODO: still have to introduce active subscription to the schema so for now its hard coded
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
