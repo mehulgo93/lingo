b333af8b99f3fc526a0f64003e42a41c80fc769e
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-04-05 14:46:31 -0400

    completing quests and leaderboard

--- app/(main)/leaderboard/page.tsx
+++ app/(main)/leaderboard/page.tsx
@@ -8,6 +8,8 @@ import {
   getUserProgress,
   getUserSubscription,
 } from "@/db/queries";
+import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const LeaderboardPage = async () => {
   const userProgressData = getUserProgress();
@@ -47,8 +49,24 @@ const LeaderboardPage = async () => {
           <p className="text-muted-foreground text-center text-lg mb-6">
             See where you stand among other learners in the community.
           </p>
+          <Separator className="mb-4 h-0.5 rounded-full " />
           {topTenUsers.map((userProgress, index) => (
-            <div key={userProgress.userId}></div>
+            <div
+              key={userProgress.userId}
+              className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
+            >
+              <p className="font-bold text-lime-700 mr-4">{index + 1}</p>
+              <Avatar className="border-bg-green-500 h-12 w-12 ml-3 mr-6">
+                <AvatarImage
+                  className="object-cover"
+                  src={userProgress.userImageSrc}
+                />
+              </Avatar>
+              <p className="font-bold text-neutral-800 flex-1">
+                {userProgress.userName}
+              </p>
+              <p className="text-muted-foreground">{userProgress.points} XP</p>
+            </div>
           ))}
         </div>
       </FeedWrapper>
